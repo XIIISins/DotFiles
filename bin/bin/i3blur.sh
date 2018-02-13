898f2d687a996ea3e60b9baed967ea82068997ef
XIIISins <xiii@xiiisins.com>
2018-02-13 10:39:37 +0100

    restructure

--- bin/bin/i3blur.sh
+++
@@ -1,9 +0,0 @@
-# How to use:
-#
-# Just place a lock.png in your home folder to overlay whatever you want
-
-#!/bin/bash
-scrot 'tmp.png' -e 'convert -blur 0x3 $f ./lockbg.png'
-convert -gravity west -composite lockbg.png ~/.lock.png lockfinal.png
-i3lock -e -d -f -i lockfinal.png
-rm lockfinal.png lockbg.png tmp.png
